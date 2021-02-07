964b16ea9dde2abc96ae0ea75e870f182734e1a6
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2021-02-07 13:25:11 +0100

    table fixed

--- src/components/VinylsList.js
+++ src/components/VinylsList.js
@@ -2,37 +2,37 @@ import React, { useEffect, useState } from "react";
 import * as config from "../config";
 
 export function VinylsList() {
-  const [vinyls, setVinyls] = useState([]);
+	const [vinyls, setVinyls] = useState([]);
 
-  useEffect(() => {
-    fetch(config.db_link + "vinyls")
-      .then((r) => r.json())
-      .then((body) => setVinyls(body));
+	useEffect(() => {
+		fetch(config.db_link + "vinyls")
+			.then((r) => r.json())
+			.then((body) => setVinyls(body));
 
-    console.log(config.db_link);
-  });
+		console.log(config.db_link);
+	});
 
-  return (
-    <div>
-      <pre>{JSON.stringify(vinyls, null, 2)}</pre>
-      <table>
-        <thead>
-          <tr>
-            <th>Autore</th>
-            <th>Titolo</th>
-          </tr>
-        </thead>
-        <tbody>
-          {vinyls.map(function (v) {
-            return (
-              <tr key={v.id}>
-                <td>{v.autore}</td>
-                <td>{v.titolo}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div>
+			{/* <pre>{JSON.stringify(vinyls, null, 2)}</pre> */}
+			<table>
+				<thead>
+					<tr>
+						<th>Autore</th>
+						<th>Titolo</th>
+					</tr>
+				</thead>
+				<tbody>
+					{vinyls.map(function (v) {
+						return (
+							<tr key={v.id}>
+								<td>{v.autore.nome}</td>
+								<td>{v.titolo}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
